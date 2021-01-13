6c96a4bed078fb913344fdf5138af2a4d3ed0247
Adrian <garciasaaib@gmail.com>
2021-01-13 13:45:10 -0700

    Webpack added, react with jsx, css loader and template of html added

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebapckPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebapckPlugin({
+      template: 'public/index.html',
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
